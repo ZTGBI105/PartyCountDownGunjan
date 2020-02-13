f819ae4e8cedfbb60cffd49787f5ccf1af4f2017
ZTGBI105 <gunjan.bhawsar.int@zehntech.com>
2020-02-13 16:01:08 +0530

    updated milstone project

--- pro2.js
+++ pro2.js
@@ -11,57 +11,61 @@ function displayclock(){
 }
 });
 
-function displayClock(){ 
+function cdFunction(){ 
 var x = setInterval(function() {
-  var partyTime =document.getElementById("partytime").value;
- 
+  // var partyTime =document.getElementById("partytime").value;
+  //  let date = new Date().getDate() + '-' + new Date().getMonth() + '-' + new Date().getFullYear();
+
+   var distance = new Date($('input[type="datetime"]').val()).getTime() - new Date().getTime();
+
+ var days=00;  
+ var hours = 00;
+ var minutes = 00 ;
+ var seconds = 00;
+
+//  console.log(new Date($('input[type="datetime"]').val()).getTime());
    
-   let date = new Date().getDate() + '-' + new Date().getMonth() + '-' + new Date().getFullYear();
+   days = Math.floor(distance / (1000 * 60 * 60 * 24)); 
+   hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-   var distance = new Date(date + " " + partyTime).getTime() - new Date().getTime();
+  
+  
+ document.getElementById("cd").innerHTML =days + ":" + hours + ":" + minutes + ":" + seconds;
 
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-
-  document.getElementById("cd").innerHTML =  hours + ":"
-  + minutes + ":" + seconds;
-
-  var modal = document.getElementById("myModal")
+  var modal = document.getElementById("myModal");
   var span =document.getElementById("close"); 
 
-  if(hours==0 && minutes==0 && seconds==0)
+  if(days<0 && hours<0 && minutes<0 && seconds<0)
   {
+    document.getElementById("cd").innerHTML="please enter the upcoming date or time";
+  }
+
+  if(days<0 && hours==0 && minutes==0 && seconds==0)
+  { 
+    
+      clearInterval(x);
+    
     var modal = document.getElementById("myModal")
     var span =document.getElementById("close"); 
   
      
        modal.style.display="block";
     
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-    window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
+    //     span.onclick = function() {
+    //     modal.style.display = "none";
+        
+    // }
+    // window.onclick = function(event) {
+    //     if (event.target == modal) {
+    //       modal.style.display = "none";
+    //     }
+    //   }
   }
 
-}, 500);
-
+}, 1000);
 
 
 }
-
-// function inCheck(){
-//     var partyTime =document.getElementById("partytime").value;
-//     console.log(partyTime);
-//     if(partyTime <= (new Date().getTime()))
-//     {
-//         document.getElementById("error").innerHTML="please enter valid time";
-//     }
-// }
-
 
